49bf894197f7b6425f4e872ae2e3b8f61db6ee91
Chinmaya Bindhani <153605374+Chinmaya-Bindhani@users.noreply.github.com>
2026-06-02 22:36:52 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
 echo "Running migrations..."
-python manage.py migrate --noinput
+python3 manage.py migrate --noinput
+
+echo "Collecting static files..."
+python3 manage.py collectstatic --noinput
 
 echo "Starting server..."
 gunicorn food_rescue_project.wsgi
